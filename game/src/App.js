28cf78fac54128986ca3a5a341ee8d1934068530
Lilit Hakobyan <lilit.hakobyan@helpsystems.com>
2018-08-07 16:51:26 +0400

    create base part

--- game/src/App.js
+++ game/src/App.js
@@ -1,21 +1,67 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+
+
 import './App.css';
 
-class App extends Component {
+const Stars =(props)=>{
+return(
+  <div className="col-5" >
+    <i className="fa fa-star" > </i>
+    <i className="fa fa-star" > </i>
+    <i className="fa fa-star" > </i> 
+    <i className="fa fa-star" > </i> 
+   </div>
+);
+}
+
+const Button =(props)=>{
+  return(
+    <div className="col-2">
+      <button className="btn btn-primary" > =</button>
+       </div>
+  );
+    
+  }
+  
+  const Answer =(props)=>{
+    return(
+      <div className="col-5">
+        .......
+         </div>
+    );
+      
+    }
+class Game extends React.Component{
+  render()
+  {
+    return (
+      <div className="row">
+        <h3> Play Nine </h3>
+        <hr />
+        <div>
+        <Stars />
+        <Button />
+        <Answer />
+        </div>
+         </div>
+    );
+  }
+}
+
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div>
+           <Game />
       </div>
     );
   }
 }
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
 
 export default App;
