ae5623b1a984de4f8ae8051dfba2523a44b7a45a
Lilit Hakobyan <lilit.hakobyan@helpsystems.com>
2018-08-08 14:15:33 +0400

    added components

--- game/src/App.js
+++ game/src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-
-
-
 import './App.css';
 
 const Stars =(props)=>{
+  const numberOfStars = 1+ Math.floor(Math.random()*9);
+
+  let stars=[];
+  for(let i=0; i<numberOfStars;i++){
+    stars.push(<i key={i} className="fa fa-star"></i>)
+  }
 return(
   <div className="col-5" >
-    <i className="fa fa-star" > </i>
-    <i className="fa fa-star" > </i>
-    <i className="fa fa-star" > </i> 
-    <i className="fa fa-star" > </i> 
+    {stars}
    </div>
 );
 }
@@ -34,18 +34,34 @@ const Button =(props)=>{
     );
       
     }
+
+    const Number =(props) =>{
+     let numbers=[];
+      for(let i=1; i<11;i++){
+      numbers.push(<span key={i} ></span>)
+  }
+      return(
+        <div className="card text-center">
+         <div>
+           {numbers}
+          </div>
+        </div>
+      )
+    }
 class Game extends React.Component{
   render()
   {
     return (
-      <div className="row">
+      <div className="container" >
         <h3> Play Nine </h3>
         <hr />
-        <div>
+        <div className="row">
         <Stars />
         <Button />
         <Answer />
         </div>
+        <br />
+        <Number />
          </div>
     );
   }
